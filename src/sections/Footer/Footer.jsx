805cdb4d4918a1f6bcc6a23e91230ac3f7d9f1c1
akshat <akshatyagyasaini@gmail.com>
2024-06-29 03:50:10 +0530

    email

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -4,6 +4,8 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
+        Contact at:akshatyagyasaini@gmail.com
+        <br></br>
         &copy; 2024 Akshat Gupta. <br />
         All rights reserved.
       </p>
